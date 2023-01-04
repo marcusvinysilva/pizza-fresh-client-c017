e9a4eb5f0a1cd5821da0db901e5aefa8297c41b2
Marcus Silva <mvsilva1997@gmail.com>
2023-01-03 22:02:35 -0300

    feat: init project and configure styled components

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,11 @@
+import { colors } from "./colors";
+import { constants } from "./constants";
+import { mixins } from "./mixins";
+
+const theme = {
+  colors,
+  constants,
+  mixins,
+} as const;
+
+export default theme;
---
+++ src/types/routes.ts
@@ -0,0 +1,8 @@
+export enum RoutePath {
+  HOME = "/",
+  LOGIN = "/login",
+  SETTINGS = "/settings",
+  SETTINGS_PRODUCTS = "products",
+  SETTINGS_TABLES = "tables",
+  SETTINGS_USERS = "users",
+}
